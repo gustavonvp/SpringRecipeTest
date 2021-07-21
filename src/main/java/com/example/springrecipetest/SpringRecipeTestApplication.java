27f88c863939d8d25a09562258bce4a79f2d2d92
gustavonvp <gustavonvp@gmail.com>
2021-07-21 02:35:37 -0300

    Initiate Project 'Takings Notes to ....'

---
+++ src/main/java/com/example/springrecipetest/SpringRecipeTestApplication.java
@@ -0,0 +1,13 @@
+package com.example.springrecipetest;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRecipeTestApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringRecipeTestApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/springrecipetest/SpringRecipeTestApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.springrecipetest;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringRecipeTestApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
