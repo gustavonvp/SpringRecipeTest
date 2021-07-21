fad32d823d758bc7ca19637b038e02cf79bc3267
gustavonvp <gustavonvp@gmail.com>
2021-07-21 03:06:17 -0300

    Creating Ingredient Class. Closes #2

--- src/main/java/com/example/springrecipetest/domain/UnitOfMeasure.java
+++ src/main/java/com/example/springrecipetest/domain/UnitOfMeasure.java
@@ -1,4 +1,32 @@
 package com.example.springrecipetest.domain;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class UnitOfMeasure {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String descripton;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescripton() {
+        return descripton;
+    }
+
+    public void setDescripton(String descripton) {
+        this.descripton = descripton;
+    }
 }
