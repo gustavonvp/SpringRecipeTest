e6ce1f3b01db2d2402a485fce77bec8d3774b013
gustavonvp <gustavonvp@gmail.com>
2021-07-23 02:56:54 -0300

    Adding JUNIT5 test in Category Class. Closes #11

---
+++ src/test/java/com/example/springrecipetest/domain/CategoryTest.java
@@ -0,0 +1,35 @@
+package com.example.springrecipetest.domain;
+
+import  org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
+import  org.junit.jupiter.api.Test;
+import  org.junit.jupiter.api.Assertions;
+
+
+class CategoryTest {
+
+    Category category;
+
+    @BeforeEach
+    public void setUp() {
+          category = new Category();
+    }
+
+    @Test
+    void getId() throws Exception {
+        Long idValue = 4L;
+        category.setId(idValue);
+        Assertions.assertEquals(idValue, category.getId());
+    }
+
+    @Test
+    void getDescription() throws Exception {
+        String descriptionValue = "Testing Properties description in category class";
+        category.setDescription(descriptionValue);
+        Assertions.assertEquals(descriptionValue, category.getDescription());
+    }
+
+    @Test
+    void setDescription() throws  Exception {
+    }
+}
