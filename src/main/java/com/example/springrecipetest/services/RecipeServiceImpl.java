79b7bd1a18642cb4aa6c04f202e0fcbcd39228f4
gustavonvp <gustavonvp@gmail.com>
2021-07-23 03:30:17 -0300

    Adding Mock test in Recipe Service. Closes #12

--- src/main/java/com/example/springrecipetest/services/RecipeServiceImpl.java
+++ src/main/java/com/example/springrecipetest/services/RecipeServiceImpl.java
@@ -2,11 +2,13 @@ package com.example.springrecipetest.services;
 
 import com.example.springrecipetest.domain.Recipe;
 import com.example.springrecipetest.repository.RecipeRepository;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 import java.util.HashSet;
 import java.util.Set;
 
+@Slf4j
 @Service
 public class RecipeServiceImpl implements RecipeService {
 
@@ -18,6 +20,8 @@ public class RecipeServiceImpl implements RecipeService {
 
     @Override
     public Set<Recipe> getRecipes() {
+        log.debug("I'm the service implementation");
+
        Set<Recipe> recipeSet = new HashSet<>();
        recipeRepository.findAll().iterator().forEachRemaining(recipeSet::add);
        return recipeSet;
