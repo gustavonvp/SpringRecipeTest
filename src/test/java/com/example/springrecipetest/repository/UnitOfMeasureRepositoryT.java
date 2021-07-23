b85cf5a096e7cb821597ace5b17c83ab5fa4c710
gustavonvp <gustavonvp@gmail.com>
2021-07-23 05:29:57 -0300

    Mock Test with JPA connection. Closes #16

---
+++ src/test/java/com/example/springrecipetest/repository/UnitOfMeasureRepositoryT.java
@@ -0,0 +1,34 @@
+package com.example.springrecipetest.repository;
+
+import com.example.springrecipetest.domain.UnitOfMeasure;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+
+import java.util.Optional;
+
+@DataJpaTest
+class UnitOfMeasureRepositoryT {
+
+    @Autowired
+    UnitOfMeasureRepository unitOfMeasureRepository;
+
+    @BeforeEach
+    public void setUp() throws Exception {
+    }
+
+    @Test
+    void findByDescription() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Teaspoon");
+        Assertions.assertEquals("Teaspoon", uomOptional.get().getDescripton());
+    }
+
+    @Test
+    void findByDescriptionCup() throws Exception {
+        Optional<UnitOfMeasure> uomOptional = unitOfMeasureRepository.findByDescription("Cup");
+        Assertions.assertEquals("Cup", uomOptional.get().getDescripton());
+    }
+}
