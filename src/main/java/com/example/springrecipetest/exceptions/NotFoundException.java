311179bc3c3087284143207293ae9d791641259a
gustavonvp <gustavonvp@gmail.com>
2021-08-05 13:41:07 -0300

    Adding Class for 400's request error's. Closes #40

---
+++ src/main/java/com/example/springrecipetest/exceptions/NotFoundException.java
@@ -0,0 +1,16 @@
+package com.example.springrecipetest.exceptions;
+
+
+//@ResponseStatus(HttpStatus.NOT_FOUND)
+public class NotFoundException extends RuntimeException{
+
+    public NotFoundException(){super();}
+
+    public NotFoundException(String message) {
+        super(message);
+    }
+
+    public NotFoundException(String message, Throwable cause) {
+        super(message, cause);
+    }
+}
