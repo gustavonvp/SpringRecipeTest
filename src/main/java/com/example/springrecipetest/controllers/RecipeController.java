8f13251bd518e7e31bf12e0761e4b32c78a525e7
gustavonvp <gustavonvp@gmail.com>
2021-08-08 20:30:18 -0300

    400 error's Recipe controller - Mocked. Closes #44

--- src/main/java/com/example/springrecipetest/controllers/RecipeController.java
+++ src/main/java/com/example/springrecipetest/controllers/RecipeController.java
@@ -70,6 +70,21 @@ public class RecipeController {
 
     }
 
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    @ExceptionHandler(NumberFormatException.class)
+    public ModelAndView handleNumberFormat(Exception exception) {
+        log.error("Handling Number Format exception");
+        log.error(exception.getMessage());
+
+        ModelAndView modelAndView = new ModelAndView();
+
+        modelAndView.setViewName("404error");
+        modelAndView.addObject("exception", exception);
+
+        return modelAndView;
+
+    }
+
 
 
 }
--- src/test/java/com/example/springrecipetest/controllers/RecipeControllerTest.java
+++ src/test/java/com/example/springrecipetest/controllers/RecipeControllerTest.java
@@ -74,6 +74,16 @@ class RecipeControllerTest {
     }
 
     @Test
+    public void testGetRecipeNumberFormatException() throws Exception {
+
+       // when(recipeService.findById(anyLong())).thenThrow(NotFoundException.class);
+
+        mockMvc.perform(get("/recipe/asdf/show"))
+                .andExpect(status().isBadRequest())
+                .andExpect(view().name("404error"));
+    }
+
+    @Test
     public void testGetNewRecipeForm() throws Exception {
         RecipeCommand command = new RecipeCommand();
 
